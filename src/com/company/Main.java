0cd57c4e18a1509bc5f8ee20011dc277bb04ec1f
Darya Gerusova <myhellsing@gmail.com>
2014-07-23 00:42:44 +0400

    lets start

---
+++ src/com/company/Main.java
@@ -0,0 +1,62 @@
+package com.company;
+
+import com.google.gdata.client.authn.oauth.*;
+import com.google.gdata.client.spreadsheet.*;
+import com.google.gdata.data.*;
+import com.google.gdata.data.batch.*;
+import com.google.gdata.data.spreadsheet.*;
+import com.google.gdata.util.*;
+
+import java.io.IOException;
+import java.net.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args)   throws AuthenticationException, MalformedURLException, IOException, ServiceException {
+
+
+
+	// write your code here
+        String USERNAME = "myhellsing@gmail.com";
+        String PASSWORD = "myfunnyfaye";
+
+        SpreadsheetService service =
+                new SpreadsheetService("MySpreadsheetIntegration-v1");
+        service.setUserCredentials(USERNAME, PASSWORD);
+
+        // TODO: Authorize the service object for a specific user (see other sections)
+
+        // Define the URL to request.  This should never change.
+        URL SPREADSHEET_FEED_URL = new URL(
+                "https://spreadsheets.google.com/feeds/worksheets/tHsrq4mO7BK-xCYKznI6IEA/private/basic");
+
+
+
+        // Make a request to the API and get all spreadsheets.
+        WorksheetFeed feed = service.getFeed(SPREADSHEET_FEED_URL, WorksheetFeed.class);
+        List<WorksheetEntry> worksheetEntries = feed.getEntries();
+
+        // Iterate through all of the spreadsheets returned
+        for (WorksheetEntry worksheetEntry : worksheetEntries) {
+            // Print the title of this spreadsheet to the screen
+            System.out.println(worksheetEntry.getTitle().getPlainText() );
+
+            URL cellFeedUrl = worksheetEntry.getCellFeedUrl();
+            CellFeed cellFeed = service.getFeed(cellFeedUrl, CellFeed.class);
+
+            // Iterate through each cell, printing its value.
+            for (CellEntry cell : cellFeed.getEntries()) {
+                // Print the cell's address in A1 notation
+                System.out.print(cell.getTitle().getPlainText() + "\t");
+                // Print the cell's address in R1C1 notation
+                System.out.print(cell.getId().substring(cell.getId().lastIndexOf('/') + 1) + "\t");
+
+                // Print the cell's displayed value (useful if the cell has a formula)
+                System.out.println(cell.getPlainTextContent() + "\t");
+            }
+            break;
+
+        }
+    }
+}
